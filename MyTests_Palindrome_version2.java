6ed4148010379abc365b633cbc57f43e1146704c
KatharinaBergen <38838546+KatharinaBergen@users.noreply.github.com>
2020-02-13 08:32:58 +0100

    Add files via upload

---
+++ MyTests_Palindrome_version2.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class MyTests_Palindrome_version2 {
+	
+	@Test
+	void test1() {
+		assertEquals(true, Palindrome_version_2_cleaner.wrapperMethod("Anna"));
+	}
+	@Test
+	void test2() {
+		assertEquals(true, Palindrome_version_2_cleaner.wrapperMethod("A n na"));
+	}
+	@Test
+	void test3() {
+		assertEquals(false, Palindrome_version_2_cleaner.wrapperMethod("Test"));
+	}
+	@Test
+	void test4() {
+		assertEquals(true, Palindrome_version_2_cleaner.wrapperMethod("?An.n!a"));
+	}
+	@Test
+	void test5() {
+		assertEquals(true, Palindrome_version_2_cleaner.wrapperMethod("Antna"));
+		
+	}
+
+}
+
+
+
+
+
